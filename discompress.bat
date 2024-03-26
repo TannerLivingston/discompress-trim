62fb8e2c80a801f83da65cc67de22f7ddacc82b8
Tanner Livingston <tannerrosslivingston@gmail.com>
2024-03-25 21:10:31 -0600

    Refactor to not create a temp file while trimming

--- discompress.bat
+++ discompress.bat
@@ -1,50 +1,122 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-REM Get the directory and file extention of the input file
+REM Get the directory and file extension of the input file
 set "input_file=%~1"
 set "input_directory=%~dp1"
 set "input_extension=%~x1"
 
-REM Get user input on start and stop times
-echo Enter clip start and end times in hh mm ss format
-echo One or both can be ignored by pressing Enter...
-echo:
-set /p "start=Enter start time: "
-set /p "end=Enter end time: "
-
-set start=%start: =:%
-set end=%end: =:%
-
 REM Extract input file name without extension
 for %%f in ("%input_file%") do set "input_file_name=%%~nf"
 
-REM Name the temp file to be used for the unprocessed clip
-set "temp_file_name=temp_%input_file_name%"
+REM Calculate clip duration
+for /f "delims=" %%i in ('ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%input_file%"') do set "duration=%%i"
+echo Original clip duration: %duration%
 
-REM Get current date and time
-for /F "tokens=1-3 delims=:." %%a in ("%time%") do (
-    set "hour=%%a"
-    set "minute=%%b"
-    set "second=%%c"
+REM Function to prompt the user for start time
+:prompt_for_start_time
+set /p "start_time=Enter start time (format: HH:MM:SS, leave blank for beginning of the video): "
+set "start_time=!start_time: =:!"
+
+REM Function to prompt the user for end time
+:prompt_for_end_time
+set /p "end_time=Enter end time (format: HH:MM:SS, leave blank for end of the video): "
+set "end_time=!end_time: =:!"
+
+REM Function to calculate duration and determine optimal bitrate
+:calculate_bitrate
+
+REM Adjust duration based on start and end times
+if defined start_time (
+    for /f "tokens=1-3 delims=:" %%a in ("%start_time%") do (
+        set /a "start_seconds=(((%%a * 60) + %%b) * 60) + %%c"
+    )
+    if defined end_time (
+        for /f "tokens=1-3 delims=:" %%a in ("%end_time%") do (
+            set /a "end_seconds=(((%%a * 60) + %%b) * 60) + %%c"
+        )
+    ) else (
+        set "end_seconds=!duration!"
+    )
+    set /a "duration=end_seconds - start_seconds"
 )
-set "current_time=%hour%%minute%%second%"
 
+REM If start time is provided without end time, set end time to the end of the video
+if defined end_time (
+    if not defined start_time (
+        set "start_seconds=0"
+        for /f "tokens=1-3 delims=:" %%a in ("%end_time%") do (
+            set /a "end_seconds=(((%%a * 60) + %%b) * 60) + %%c"
+        )
+        set /a "duration=end_seconds"
+    )
+)
+
+REM Calculate target bitrate based on clip duration
+set /a "bitrate=25 * 8 * 1000 / duration"
+echo Clip duration: %duration%s
+echo Target bitrate: %bitrate%k
+
+REM Check if the target bitrate is under 150kbps
+if %bitrate% LSS 150 (
+    echo Target bitrate is under 150kbps.
+    echo Unable to compress.
+    pause
+    goto :eof
+)
+
+REM Calculate video and audio bitrates
+set /a "video_bitrate=bitrate * 90 / 100"
+set /a "audio_bitrate=bitrate * 10 / 100"
+
+REM Check if the video bitrate is under 125kbps
+if %video_bitrate% LSS 125 (
+    echo Target video bitrate is under 125kbps.
+    echo Unable to compress.
+    pause
+    goto :eof
+)
+
+REM Check if the audio bitrate is under 32kbps
+if %audio_bitrate% LSS 32 (
+    echo Target audio bitrate is under 32kbps.
+    echo Unable to compress.
+    pause
+    goto :eof
+)
+
+REM Function to trim and compress video using FFmpeg
+:trim_and_compress_video
+echo Trimming and compressing video file: %input_file%
+set "output_file=25MB_%input_file_name%.mp4"
+pushd %input_directory%
+set "ffmpeg_command=ffmpeg -hide_banner -loglevel warning -stats"
+
+REM Add start time parameter if provided
+if defined start_time (
+    set "ffmpeg_command=!ffmpeg_command! -ss %start_time%"
+)
+
+REM Add end time parameter if provided
+if defined end_time (
+    set "ffmpeg_command=!ffmpeg_command! -to %end_time%"
+)
+
+set "ffmpeg_command=!ffmpeg_command! -i "%input_file%" -preset slow -c:v h264_nvenc -b:v %video_bitrate%k -c:a aac -b:a %audio_bitrate%k -bufsize %bitrate%k -minrate 100 -maxrate %bitrate%k "%output_file%"
+echo %ffmpeg_command%
+!ffmpeg_command!
+popd
+echo Compression complete. Output file: %output_file%
+goto :eof
 
 
 REM Check if input file extension is valid for video
 set "video_extensions=.mp4 .avi .mkv .mov .flv .wmv .webm .mpeg .3gp"
 echo %video_extensions% | find /i "%input_extension%" >nul && (
-    call :trim_video
-    call :process_video
-    call :delete_temp_file
-    goto :eof
-)
-
-REM Check if input file extension is valid for audio
-set "audio_extensions=.mp3 .wav .m4a .flac .aac .ogg .wma"
-echo %audio_extensions% | find /i "%input_extension%" >nul && (
-    call :process_audio
+    call :prompt_for_start_time
+    call :prompt_for_end_time
+    call :calculate_bitrate
+    call :trim_and_compress_video
     goto :eof
 )
 
@@ -53,120 +125,3 @@ echo File type not supported.
 echo Terminating compression.
 pause
 goto :eof
-
-REM Function for trimming video file
-:trim_video (
-    echo:
-    echo Trimming clip from video
-
-    REM Use FFMPEG to trim the video clip
-    pushd %input_directory%
-    set "trim_command=ffmpeg -hide_banner -loglevel warning -i "%input_file%""
-    set "start_arg="
-    set "end_arg="
-    if %start% NEQ  =: (
-        set "start_arg=-ss %start%"
-    )
-    if %end% NEQ  =: (
-        set "end_arg=-to %end%"
-    )
-    set "trim_command=%trim_command% %start_arg% %end_arg% -c copy "%temp_file_name%.mp4""
-    %trim_command%
-
-    REM ffmpeg -hide_banner -loglevel warning -i "%input_file%" -ss %start% -to %end% -c copy "%temp_file_name%.mp4"
-    popd
-
-    goto :eof
-)
-
-REM Function for deleting the temp file
-:delete_temp_file (
-    echo Removing temp file
-
-    pushd %input_directory%
-    del "%temp_file_name%.mp4"
-
-    goto :eof
-)
-
-REM Function for processing video file
-:process_video (
-    echo Processing video file: %input_file%
-
-    REM Calculate bitrate based on input file duration
-    for /f "delims=" %%i in ('ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%temp_file_name%.mp4"') do set "duration=%%i"
-
-    set /a "bitrate=23 * 8 * 1000 / duration"
-    echo Video length: %duration%s
-    echo Bitrate target: %bitrate%k
-
-    REM Exit if target bitrate is under 150kbps
-    if %bitrate% LSS 150 (
-    echo Target bitrate is under 150kbps.
-    echo Unable to compress.
-    pause
-    goto :eof
-    )
-
-    REM Allocate bitrate based on video properties
-    set /a "video_bitrate=bitrate * 90 / 100"
-    set /a "audio_bitrate=bitrate * 10 / 100"
-
-    echo Video Bitrate: %video_bitrate%
-    echo Audio Bitrate: %audio_bitrate%
-
-
-
-    REM Exit if target video bitrate is under 125kbps
-    if %video_bitrate% LSS 125 (
-        echo Target video bitrate is under 125kbps.
-        echo Unable to compress.
-        pause
-        goto :eof
-    )
-
-    REM Exit if target audio bitrate is under 32kbps
-    if %audio_bitrate% LSS 32 (
-        echo Target audio bitrate is under 32.
-        echo Unable to compress.
-        pause
-        goto :eof
-    )
-
-    pushd %input_directory%
-    echo Compressing video file using FFmpeg...
-    ffmpeg -hide_banner -loglevel warning -stats -threads 0 -hwaccel auto -i "%temp_file_name%.mp4" -preset slow -c:v h264_nvenc -b:v %video_bitrate%k -c:a aac -b:a %audio_bitrate%k -bufsize %bitrate%k -minrate 100 -maxrate %bitrate%k "25MB_%input_file_name%.mp4"
-    popd
-
-    goto :eof
-)
-
-
-REM Function for processing audio file
-:process_audio (
-    echo Processing audio file: %input_file%
-
-    REM Calculate input file duration
-    for /f "delims=" %%i in ('ffprobe -v error -show_entries format^=duration -of default^=noprint_wrappers^=1:nokey^=1 "%temp_file_name%.mp4"') do set "duration=%%i"
-
-    REM Calculate target bitrate based on input file duration
-    set /a "bitrate=25 * 8 * 1000 / duration"
-    echo Audio duration: %duration%s
-    echo Bitrate target: %bitrate%k
-
-    REM Exit if target bitrate is under 32kbps
-    if %bitrate% LSS 32 (
-        echo Target bitrate is under 32kbps.
-        echo Unable to compress.
-        pause
-        goto :eof
-    )
-
-    REM Compress audio file using FFmpeg
-    pushd %input_directory%
-    echo Compressing audio file using FFmpeg...
-    ffmpeg -hide_banner -loglevel warning -stats -i "%temp_file_name%.mp4" -preset slow -c:a libmp3lame -b:a %bitrate%k -bufsize %bitrate%k -minrate 100 -maxrate %bitrate%k "25MB_%input_file_name%.mp3"
-    popd
-
-    goto :eof
-)
